53371b9292aae5401653606eaf4ccea60670895b
sankar566 <60536375+sankar566@users.noreply.github.com>
2020-09-11 07:37:31 -0700

    Add files via upload

---
+++ 08-jsp-b2-redirect/src/redirect/first.java
@@ -0,0 +1,19 @@
+package redirect;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class first extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		resp.sendRedirect("http://www.google.com");
+		resp.sendRedirect("RedirectTest.html");
+	}
+
+
+}
