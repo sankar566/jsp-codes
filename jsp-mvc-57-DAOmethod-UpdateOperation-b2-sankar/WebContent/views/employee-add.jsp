eada4d9ec0ea318954fd64fe4df5ea7844b0a4fb
sankar566 <60536375+sankar566@users.noreply.github.com>
2020-09-11 08:25:14 -0700

    Add files via upload

---
+++ jsp-mvc-57-DAOmethod-UpdateOperation-b2-sankar/WebContent/views/employee-add.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+</head>
+<body>
+<div class="container">
+
+ <form action = "${pageContext.request.contextPath}/EmployeeController" method="POST">
+ 
+  Enter name: <input type="text" name = "name" value="${employee.name}"/> </br>
+  Enter date of birth:<input type="date" name="dob" value="${employee.dob}"/> </br>
+  Enter department:<input type="text" name="department" value="${employee.department}"/> </br>
+  <input type="hidden" name="id" value="${employee.id}"/>
+  <button class = "btn btn-primary">Save Employee</button>
+ </form>
+
+</div>
+
+</body>
+</html>
---
+++ jsp-mvc-57-DAOmethod-UpdateOperation-b2-sankar/src/EmployeeDAO/sankar/EmployeeDAOImpl.java
@@ -0,0 +1,122 @@
+package EmployeeDAO.sankar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import DBConnectionUtil.sankar.DBConnectionUtil;
+import Entity.sankar.Employee;
+
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+	
+	Connection connection = null;
+	ResultSet resultSet = null;
+	Statement statement = null;
+	PreparedStatement preparedStatement = null;
+
+	 
+	@Override
+	public List<Employee> get() {
+		
+		List<Employee> list = null;
+		Employee employee = null;
+		
+		try {
+			
+			list = new ArrayList<Employee>();
+			//create the sql query
+			String sql = "SELECT * FROM tbl_employee";
+			// get the database connection
+			connection = DBConnectionUtil.openConnection();
+			// create the statement
+			statement = connection.createStatement();
+			// Execute the sql query
+			resultSet = statement.executeQuery(sql);
+			//process the result set
+			while(resultSet.next()) {
+				employee = new Employee();
+				employee.setId(resultSet.getInt("id"));
+				employee.setName(resultSet.getString("name"));
+				employee.setDepartment(resultSet.getString("department"));
+				employee.setDob(resultSet.getString("dob"));
+				// Add employee to list
+				list.add(employee);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		//return the list
+		return list;
+	}
+
+
+	@Override
+	public boolean Save(Employee e) {
+		boolean flag = false;
+		try {
+			String sql = "INSERT INTO tbl_employee(name,dob,department)VALUES('"+e.getName()+"','"+e.getDob()+"','"+e.getDepartment()+"')";
+			connection = DBConnectionUtil.openConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			flag = true;
+			
+		}
+		catch(SQLException ex){
+			ex.printStackTrace();
+		}
+		return false;
+	}
+
+
+	@Override
+	public Employee get(int id) {
+		Employee employee = null;
+		try {
+			employee = new Employee();
+			String sql = "SELECT * FROM tbl_employee WHERE id="+id;			
+			connection = DBConnectionUtil.openConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+			while(resultSet.next()) {
+				employee = new Employee(); 
+				employee.setId(resultSet.getInt("id"));
+				employee.setName(resultSet.getString("name"));
+				employee.setDepartment(resultSet.getString("department"));
+				employee.setDob(resultSet.getString("dob"));
+			}
+		} catch (SQLException ex ) {
+			ex.printStackTrace();
+		}
+		System.out.println("Employee name:" +employee.getName()+ "Employee date of birth :" +employee.getDob()+ "Employee department:" +employee.getDepartment() );
+		return employee;
+	}
+
+
+	@Override
+	public boolean update(Employee e) {
+		boolean flag = false;
+		try {
+			String sql = "UPDATE tbl_employee SET name = '"+e.getName()+"', "
+					+ "department = '"+e.getDepartment()+"', dob = '"+e.getDob()+"' where id="+e.getId();
+			connection = DBConnectionUtil.openConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			flag = true;
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		return flag;
+		
+	}
+
+
+	
+
+
+
+} 
