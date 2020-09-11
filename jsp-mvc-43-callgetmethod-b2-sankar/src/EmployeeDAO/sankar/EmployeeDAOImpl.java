02d24f4fd4ea2a0e264a1f74b45e583ccf60814c
sankar566 <60536375+sankar566@users.noreply.github.com>
2020-09-11 08:04:36 -0700

    Add files via upload

---
+++ jsp-mvc-43-callgetmethod-b2-sankar/src/EmployeeDAO/sankar/EmployeeDAOImpl.java
@@ -0,0 +1,56 @@
+package EmployeeDAO.sankar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import DBConnectionUtil.sankar.DBConnectionUtil;
+import Entity.sankar.Employee;
+
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+	
+	Connection connection = null;
+	ResultSet resultSet = null;
+	Statement statement = null;
+	
+	 
+	@Override
+	public List<Employee> get() {
+		
+		List<Employee> list = null;
+		Employee employee = null;
+		
+		try {
+			
+			list = new ArrayList<Employee>();
+			//create the sql query
+			String sql = "SELECT * FROM tbl_employee";
+			// get the database connection
+			connection = DBConnectionUtil.openConnection();
+			// create the statement
+			statement = connection.createStatement();
+			// Execute the sql query
+			resultSet = statement.executeQuery(sql);
+			//process the result set
+			while(resultSet.next()) {
+				employee = new Employee();
+				employee.setId(resultSet.getInt("id"));
+				employee.setName(resultSet.getString("name"));
+				employee.setDepartment(resultSet.getString("department"));
+				employee.setDob(resultSet.getString("dob"));
+				// Add employee to list
+				list.add(employee);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		//return the list
+		return list;
+	}
+
+}
